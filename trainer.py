6803ceab998adb55ca212067e5593cd10ba3b1d4
Khai Duc <hzpro1221>
2024-03-27 09:06:53 +0700

    add

--- trainer.py
+++ trainer.py
@@ -12,25 +12,46 @@ if __name__ == '__main__':
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+    setup = input("Already has checkpoint and history_file? (yes/no)")
     setting = input("Chose setting (freeze/continue/new):")
     if (setting=="freeze"):
         model = Model(device=device, freeze=True).to(device)
+
+        if (setup=="yes"):
+            model.load_checkpoint()
+            for param in model.conditioner.encoder.parameters():
+                param.requires_grad = False
+
         num_eps = 3
     if (setting=="continue"):
-        model = Model(device=device).to(device)            
-        model.load_checkpoint()
+        model = Model(device=device).to(device)
+
+        if (setup=="yes"):
+            model.load_checkpoint()
 
         for param in model.conditioner.encoder.parameters():
             param.requires_grad = True
 
         num_eps = 5
-    if (setting=="new"):
-        model = Model(device=device).to(device)
-        num_eps = 10
     print(f"training with setting: {setting}")
     print(f"loading data...")
     with open("/content/Program_Synthesizer/data_manimML.json") as f:
-        train_data = json.load(f)
+        data = json.load(f)
+
+    print(f"loading history file...")
+    if (setup=="yes"):
+        with open("/content/drive/MyDrive/program_synthesizer/history.json") as f:
+            history = json.load(f)
+        
+        train_adapter_epochs = history["train_adapter_epochs"]
+        train_adapter_losses = history["train_adapter_losses"]
+        train_epochs = history["train_epochs"]
+        train_losses = history["train_losses"]
+    else:
+        train_adapter_epochs = 0
+        train_adapter_losses = []
+        train_epochs = 0
+        train_losses = []
 
     # Hyperparameter
     batch_size = 8
@@ -40,13 +61,24 @@ if __name__ == '__main__':
     loss = nn.CrossEntropyLoss()
 
     optim = AdamW(model.parameters(), lr=lr)
-    processed_data = dataprocess(train_data)
-    processed_data = CustomDataset(processed_data) 
-    
-    dataloader = DataLoader(processed_data, batch_size=batch_size, shuffle=True, collate_fn=custom_collate_func, drop_last=True)
+    processed_data = dataprocess(data)
+
+    training_len = round(len(processed_data) * 0.9)
+
+    train_data = processed_data[:training_len]
+    val_data = processed_data[training_len:]
+
+    train_data = CustomDataset(train_data) 
+    val_data = CustomDataset(val_data)
+    train_dataloader = DataLoader(processed_data, batch_size=batch_size, shuffle=True, collate_fn=custom_collate_func, drop_last=True)
+    val_dataloader = DataLoader(processed_data, batch_size=1, collate_fn=custom_collate_func, drop_last=True)
     print("start training")
     for epoch in range(num_eps):
-        for i, batch in enumerate(dataloader):
+        num_batch_train = 0
+        num_batch_val = 0
+        train_loss = 0
+        val_loss = 0
+        for i, batch in enumerate(train_dataloader):
             optim.zero_grad()
             logits = model.forward(text=batch["text"], prev_func_list=batch["prev_func_list"])
 
@@ -54,7 +86,36 @@ if __name__ == '__main__':
             loss_value = loss(logits.float(), label.float())
             loss_value.backward()
 
-            print(f"Epoch: {epoch}, Batch: {i}, loss: {loss_value}")
+            train_loss += loss_value
+            num_batch_train += 1
+            
+            print(f"Epoch: {epoch}, step: {i}, loss: {loss_value}")
             optim.step()
+
+        mean_loss_train = train_loss/num_batch_train
+
+        for i, batch in enumerate(val_dataloader):
+            logits = model.forward(text=batch["text"], prev_func_list=batch["prev_func_list"])
+
+            label = torch.tensor(batch["label"]).to(device)
+            loss_value = loss(logits.float(), label.float())
+
+            val_loss += loss_value
+            num_batch_val += 1
+        
+        mean_loss_val = val_loss/num_batch_val
+
+        if (setting=="freeze"):
+            train_adapter_epochs += 1
+            train_adapter_losses.append(mean_loss_train)
+        if (setting=="continue"):
+            train_epochs += 1
+            train_losses.append(mean_loss_val)
+
     print("save checkpoint")
     model.save_checkpoint()
+
+    print("save history training")
+    history = {"train_adapter_epochs": train_adapter_epochs, "train_adapter_losses": train_adapter_losses, "train_epochs": train_epochs, "train_losses": train_losses}
+    with open("/content/drive/MyDrive/program_synthesizer/history.json", "w") as outfile:
+        outfile.write(history)
